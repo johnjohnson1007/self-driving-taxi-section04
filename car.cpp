2ac0438098c05ff9a9a5b9ea4ca3b6677167decb
Unknown <johnjohnson@my.uri.edu>
2017-11-20 15:05:05 -0500

    Added functionality for Read Speed function
    Now the Read Speed function will return the cars current speed as an integer.

--- car.cpp
+++ car.cpp
@@ -33,7 +33,7 @@ int Car::read_battery()
 
 int Car::read_speed()
 {
-    return -10;
+    return speed;
 }
 
 Car &Car::shift()
@@ -61,12 +61,12 @@ bool Car::check_distance(std::vector<int> pickup, std::vector<int> dropoff)
     return true;
 }
 
-bool Car::is_home() 
+bool Car::is_home()
 {
     return false;
 }
 
-bool Car::has_passengers() 
+bool Car::has_passengers()
 {
     return false;
 }
